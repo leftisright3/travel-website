5caa2550cdceaea246ddd374d64220fe052516aa
Kushan Patel <kushanpatelcmu@gmail.com>
2018-11-26 15:03:35 -0800

    added more data/content. more pictures

--- src/main/webapp/src/app/objects/entries.ts
+++ src/main/webapp/src/app/objects/entries.ts
@@ -9,7 +9,7 @@ export const ENTRIES: Entry[] = [
         title: '',
         heading: '',
         subHeading: '',
-        videoLink: 'https://www.youtube.com/embed/rqNTdFhWpJE',
+        videoLink: 'https://www.youtube.com/embed/GiRlYbNAES0',
         imageUrl: 'assets/img/glacier.jpg',
         content: 'We landed in Geneva on a Wedesday. Took a train from Geneva to Verbier. Gondola ride from the bottom of the mountain to the base of the resort. ' +
           'One whole day of snowboarding and that\'s it. I suffered an injury in the afternoon from a bad fall and had to call it a day early.'
@@ -35,8 +35,8 @@ export const ENTRIES: Entry[] = [
         title: '',
         heading: '',
         subHeading: '',
-        videoLink: 'https://www.youtube.com/embed/rqNTdFhWpJE',
-        imageUrl: 'assets/img/cayman_scuba.jpg',
+        videoLink: 'https://www.youtube.com/embed/FDlvubr5CQE',
+        imageUrl: 'assets/img/sky_sunset.jpg',
         content: ''
       }
     ],
@@ -47,7 +47,7 @@ export const ENTRIES: Entry[] = [
     title: 'Grand Cayman Islands',
     heading: 'My first ever international trip!',
     description: 'Tropical adventures include a jet ski safari tour, dolphin encounter, and scuba diving a WWII wreck!',
-    thumbnail: 'assets/img/cayman_scuba.jpg'
+    thumbnail: 'assets/img/sky_sunset.jpg'
   },
 
 
@@ -59,6 +59,7 @@ export const ENTRIES: Entry[] = [
         title: '',
         heading: '',
         subHeading: '',
+        videoLink: 'https://www.youtube.com/embed/PHZJvQZk2Yw',
         imageUrl: 'assets/img/cayman_sunset.jpg',
         content: ''
       }
@@ -83,6 +84,7 @@ export const ENTRIES: Entry[] = [
         title: '',
         heading: '',
         subHeading: '',
+        videoLink: 'https://www.youtube.com/embed/PHZJvQZk2Yw',
         imageUrl: 'assets/img/maine.jpg',
         content: ''
       }
@@ -95,5 +97,149 @@ export const ENTRIES: Entry[] = [
     heading: 'Granite, Half-Dome, and Waterfalls',
     description: 'The Half Dome Cable Route hike runs from the valley floor to the top of the dome in 8.2 mi (13 km), with 4,800 ft (1,460 m) of elevation gain.',
     thumbnail: 'assets/img/maine.jpg'
+  },
+
+
+  {
+    id: 4,
+    name: 'Cabo San Lucas',
+    sections: [
+      {
+        title: '',
+        heading: '',
+        subHeading: '',
+        videoLink: 'https://www.youtube.com/embed/rqNTdFhWpJE',
+        imageUrl: 'assets/img/cayman_scuba.jpg',
+        content: ''
+      }
+    ],
+    location: 'Cabo San Lucas',
+    pictures: '',
+    latitude: 123,
+    longitude: 321,
+    title: 'Cabo San Lucas',
+    heading: 'Sharks, Whales, and Dolphins',
+    description: 'Two-day, four-tank scuba diving trip to see a large school of hammerhead sharks',
+    thumbnail: 'assets/img/cayman_scuba.jpg'
+  },
+
+
+  {
+    id: 5,
+    name: 'Vancouver',
+    sections: [
+      {
+        title: '',
+        heading: '',
+        subHeading: '',
+        videoLink: 'https://www.youtube.com/embed/0jmuhorkdDY',
+        imageUrl: 'assets/img/snowboard.jpg',
+        content: ''
+      }
+    ],
+    location: 'Vancouver',
+    pictures: '',
+    latitude: 123,
+    longitude: 321,
+    title: 'Vancouver',
+    heading: 'Winter Wonderland',
+    description: 'Weekend snowboarding trip to the best ski resort in North America - Whistler',
+    thumbnail: 'assets/img/snowboard.jpg'
+  },
+
+
+  {
+    id: 6,
+    name: 'Grand Canyon & Zion National Park',
+    sections: [
+      {
+        title: '',
+        heading: '',
+        subHeading: '',
+        videoLink: 'https://www.youtube.com/embed/7x1KExk52xg',
+        imageUrl: 'assets/img/colorado.jpg',
+        content: ''
+      }
+    ],
+    location: 'Grand Canyon & Zion National Park',
+    pictures: '',
+    latitude: 123,
+    longitude: 321,
+    title: 'Grand Canyon & Zion National Park',
+    heading: 'Red Sandstone and Cliffs',
+    description: 'Highlight being the Angel\'s Landing hike and The Narrows. RV\'ing across the country was also a lot of fun',
+    thumbnail: 'assets/img/colorado.jpg'
+  },
+
+
+  {
+    id: 7,
+    name: 'Washington DC',
+    sections: [
+      {
+        title: '',
+        heading: '',
+        subHeading: '',
+        videoLink: 'https://www.youtube.com/embed/PHZJvQZk2Yw',
+        imageUrl: 'assets/img/mic.jpg',
+        content: ''
+      }
+    ],
+    location: 'Washington DC',
+    pictures: '',
+    latitude: 123,
+    longitude: 321,
+    title: 'Washington DC',
+    heading: 'Butter - A Tribe Called Quest',
+    description: 'For your hilarity and entertainment',
+    thumbnail: 'assets/img/mic.jpg'
+  },
+
+
+  {
+    id: 8,
+    name: 'San Diego',
+    sections: [
+      {
+        title: '',
+        heading: '',
+        subHeading: '',
+        videoLink: 'https://www.youtube.com/embed/PHZJvQZk2Yw',
+        imageUrl: 'assets/img/cpt_morgan_boat.jpg',
+        content: ''
+      }
+    ],
+    location: 'San Diego',
+    pictures: '',
+    latitude: 123,
+    longitude: 321,
+    title: 'San Diego',
+    heading: 'Adrenaline Junkie',
+    description: 'For your hilarity and entertainment',
+    thumbnail: 'assets/img/cpt_morgan_boat.jpg'
+  },
+
+
+  {
+    id: 9,
+    name: 'Mumbai',
+    sections: [
+      {
+        title: '',
+        heading: '',
+        subHeading: '',
+        videoLink: 'https://www.youtube.com/embed/PHZJvQZk2Yw',
+        imageUrl: 'assets/img/indian_dress.jpg',
+        content: ''
+      }
+    ],
+    location: 'Mumbai',
+    pictures: '',
+    latitude: 123,
+    longitude: 321,
+    title: 'Mumbai',
+    heading: 'Tanushri\'s and Vivek\'s Wedding',
+    description: 'The first wedding of our college crew, this one was legendary because everybody showed up.',
+    thumbnail: 'assets/img/wedding_group.jpg'
   }
 ];
